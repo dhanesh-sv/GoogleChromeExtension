79c1afd6eb3ba4b6c2d493740d222e5d349c8727
DhaneshS <dhanesh.sv@gmail.com>
2017-06-06 09:41:44 +0530

    testChange

--- popup.js
+++ popup.js
@@ -92,7 +92,8 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    debugger;
   getCurrentTabUrl(function(url) {
     // Put the image URL in Google search.
     renderStatus('Performing Google Image search for ' + url);
